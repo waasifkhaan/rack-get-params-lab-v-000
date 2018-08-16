ca3b1aaa8aa222a00f26624ded934efb7de7c550
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 22:25:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,6 @@ class Application
         @@cart.each do |cart|
           res.write "#{cart}\n"
       end 
-    end 
     elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
