4b45b009975f44b166f9325a95884a4ba0fb07da
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 23:07:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,11 +22,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
-      search_term = req.params["item"]
-      if @@items.include?(search_term)
-        @@items.each do |item|
-          @@cart << search_term if item == search_term
-        end   
+      add_item = req.params["item"]
+      if @@items.include?(add_item)
+        @@cart << add_item 
       else 
         resp.write "Item not found"
       end 
