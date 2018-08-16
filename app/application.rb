19f152a5a6f3ab6a087a5cdaa067be987deda95e
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 22:21:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    if req.path.match(/cart/)
+      if @@cart.empty? 
+        resp.write
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
